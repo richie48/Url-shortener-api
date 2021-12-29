0608c45f96ca382827652fcac626a1b5bc999bb5
richie48 <odiorrichard@gmail.com>
2021-12-29 23:22:54 +0000

    connected to db

--- config/db.js
+++ config/db.js
@@ -2,4 +2,15 @@ const mongoose=require('mongoose')
 
 //to setup my database connection
 
+//Not using process dotenv for this buil
+const connectDb =async () =>{
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`mongodb database running on port`)
+}
+
+module.exports=connectDb;
+
 
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express =require('express')
+const dotenv=require('dotenv')
 
 const urlRoute=require('./routes/urlRoute')
 
 const app=express()
 
+dotenv.config({ path: './config/config.env' });
 
 const port=5000
 
 
 
-app.use(urlRoute)
+app.use('/api/urlroute',urlRoute)
 
 
 app.listen(port,console.log(`server running on port ${port}`))
